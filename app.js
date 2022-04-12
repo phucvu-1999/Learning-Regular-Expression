f36266f91b5a9004698da4cafac7d92371c8d094
Tien Phuc <leesinvn123@gmail.com>
2022-04-12 21:58:01 +0700

    Learning test method and characters class

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ function santitize(phoneNumberArr) {
   return phoneNumberArr.map((str) => str.replace(/[. -]/g, ""));
 }
 
-console.log("the phone number after santize: ", santitize(phoneNumber));
+// Test method of reg
+
+const text01 = `Hello hello HeLlo heLLO hello my name is Tien Phuc and my phone number is 038-699-1929 or 038 699 1929. My email is tienphuc@futurify.vn`;
+
+const pattern = /hello/gi;
+
+const result = text01.match(pattern);
+
+// Test method;
+
+const number1 = new RegExp("^0[0-9]{9}$");
+const testNum1 = "0123456789";
+
+console.log(number1.test(testNum1));
