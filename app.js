5b6e26ee13bbf792ac47ab944094ee1b6afc3ec4
Tien Phuc <leesinvn123@gmail.com>
2022-04-12 20:26:55 +0700

    Basic regular expression

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const phoneNumber = ["097.123.1234", "091-303-0000", "0123 0123 0123"];
+
+function santitize(phoneNumberArr) {
+  return phoneNumberArr.map((str) => str.replace(/[. -]/g, ""));
+}
+
+console.log("the phone number after santize: ", santitize(phoneNumber));
